00fb0d09eee55d3aae34e64ec3eb7b2d87be8caf
githubfoam <lequestionis@gmail.com>
2018-11-23 03:13:01 +0300

    bash updated

--- somescript.sh
+++ somescript.sh
@@ -1,3 +1,4 @@
 
 #!/bin/sh
-curl -H "Content-Type: application/json" --data '{"source_type":"Branch", "source_name":"ubuntu-18.04"}' && \ -X POST https://registry.hub.docker.com/u/dockerfoam/onos/trigger/$(DOCKER_TOKEN)/
+echo "curl testing"
+#curl -H "Content-Type: application/json" --data '{"source_type":"Branch", "source_name":"ubuntu-18.04"}' && \ -X POST https://registry.hub.docker.com/u/dockerfoam/onos/trigger/$(DOCKER_TOKEN)/
