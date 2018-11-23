aede8a7a03b13ab95159962b4d46efea2d95061b
githubfoam <lequestionis@gmail.com>
2018-11-23 03:04:35 +0300

    docker build trigger stage added

--- somescript.sh
+++ somescript.sh
@@ -1,3 +1,3 @@
 
 #!/bin/sh
-echo "tester "
+curl -H "Content-Type: application/json" --data '{"source_type":"Branch", "source_name":"ubuntu-18.04"}' && \ -X POST https://registry.hub.docker.com/u/dockerfoam/onos/trigger/$(DOCKER_TOKEN)/
