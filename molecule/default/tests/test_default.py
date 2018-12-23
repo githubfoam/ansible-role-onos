fd2b96ed196f629419b894ebb070df61f6979830
githubfoam <lequestionis@gmail.com>
2018-12-23 17:36:01 +0300

    verify added

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,5 +1,5 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -11,3 +11,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 # assert f.exists
 # assert f.user == 'root'
 # assert f.group == 'root'
+
+@pytest.mark.parametrize('pkg', [
+  'software-properties-common',
+  'oracle-java8-set-default',
+  'curl',
+  'oracle-java8-installer'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
