b220847102be68137edcec03ff63ba7daba5af03
githubfoam <lequestionis@gmail.com>
2018-12-26 23:38:46 +0300

    ubuntu1804

---
+++ molecule/ubuntu1804/tests/debian/test_ubuntu1804.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.parametrize('pkg', [
+  'software-properties-common',
+  'oracle-java8-set-default',
+  'curl',
+  'oracle-java8-installer'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
