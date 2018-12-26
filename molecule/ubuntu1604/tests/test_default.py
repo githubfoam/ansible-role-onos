a7386b3c04ba0499f36a66eeb7f440802610ee9c
githubfoam <lequestionis@gmail.com>
2018-12-27 01:13:17 +0300

    tests

--- molecule/ubuntu1604/tests/test_default.py
+++ molecule/ubuntu1604/tests/test_default.py
@@ -1,3 +1,13 @@
 import pytest
 
 testinfra_hosts = ["ubuntu1604"]
+
+@pytest.mark.parametrize('pkg', [
+  'software-properties-common',
+  'oracle-java8-set-default',
+  'curl',
+  'oracle-java8-installer'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
