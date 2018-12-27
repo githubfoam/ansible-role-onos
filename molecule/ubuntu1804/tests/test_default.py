d0ee307dcb68b174c61da204b219d4f05592aeff
githubfoam <lequestionis@gmail.com>
2018-12-27 11:42:36 +0300

    systemd fix 4 bionic

--- molecule/ubuntu1804/tests/test_default.py
+++ molecule/ubuntu1804/tests/test_default.py
@@ -1,1 +1,15 @@
-testinfra_hosts = ["ubuntu1804"]
+import pytest
+
+
+testinfra_hosts = ["bionic"]
+
+
+@pytest.mark.parametrize('pkg', [
+  'software-properties-common',
+  'oracle-java8-set-default',
+  'curl',
+  'oracle-java8-installer'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
